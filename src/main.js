ef6bcb0c42bd41cc4093c46a0824f7c41e58ac31
Yaroslav Pisarev <yaricp@gmail.com>
2021-12-25 22:31:12 +0700

    added pause and button clear

--- src/main.js
+++ src/main.js
@@ -2,20 +2,19 @@ var audioContext = new AudioContext();
 var game_text = null;
 var measurement = null;
 var gauge = null;
-var myChart = null;
+var chartAxis = null;
+var chartMod = null;
 var acl = null;
 var speedCalculator = null;
 var quotes = ["Punch not detected", "Great punch!", "Roll with the punches!"];
 
 
 // Calculates the *first* velocity peak about X axis, or exiting on timeout.
-class MaxSpeedCalculator {
- constructor(linearAccel, onresult, onpunchdetected, timeout /*in ms*/) {
+class MainCalculator {
+ constructor(linearAccel, onresult, timeout /*in ms*/) {
    this.accel = linearAccel;
    this.measuring = false;
    this.onresult = onresult;
-   this.onpunchdetected = onpunchdetected;
-   this.punchDetected = false;
    this.maxSpeed = 0;
 
    this.dt_list = [];
@@ -29,18 +28,25 @@ class MaxSpeedCalculator {
    this.az_data = [];
    this.vz_data = [];
 
-   this.vx = 0; // Velocity at time t.
    this.ax = 0;
-   this.vy = 0; // Velocity at time t.
+   this.vx = 0; // Velocity at time t.
+   this.lx = 0;
+
    this.ay = 0;
-   this.vz = 0; // Velocity at time t.
+   this.vy = 0; // Velocity at time t.
+   this.ly = 0;
+
    this.az = 0;// Acceleration at time t.
+   this.vz = 0; // Velocity at time t.
+   this.lz = 0;
+
    this.mod_a = 0;
    this.mod_v = 0;
    this.t = 0;
    this.list_data_axis = {
     a: {x:[], y:[], z:[]},
-    v: {x:[], y:[], z:[]}
+    v: {x:[], y:[], z:[]},
+    l: {x:[], y:[], z:[]}
    }
    this.accel_mod = [];
    this.velocity_mod = [];
@@ -51,10 +57,13 @@ class MaxSpeedCalculator {
    function measure_axis(axis, dt) {
      //setGameText(axis+ " - "+ this.accel[axis]+ " - "+ this['v'+axis]);
      this.list_data_axis.a[axis].push(this.accel[axis]);
-     let v = this['v'+axis] + (this.accel[axis] + this['a' + axis]) / 2 * dt;
+     let v = this['v' + axis] + (this.accel[axis] + this['a' + axis]) / 2 * dt;
+     let l  = this['l' + axis] + (v + this['v' + axis]) / 2 * dt;
      this.list_data_axis.v[axis].push(v);
+     this.list_data_axis.l[axis].push(l);
      this['a'+axis] = this.accel[axis];
      this['v'+axis] = v;
+     this['l'+axis] = l;
    }
 
    function onreading() {
@@ -79,6 +88,7 @@ class MaxSpeedCalculator {
        this.stop();
        this.onresult();
      }
+
    }
    this.measure_axis = measure_axis.bind(this);
    this.onreading = onreading.bind(this);
@@ -91,47 +101,66 @@ class MaxSpeedCalculator {
    return Math.round(this.maxSpeed * kmPerHourCoef);
  }
 
- start() {
-   if (this.accel.timestamp === null) {
-     console.error("accelerometer must have initial values");
-     return;
-   }
-
-   if (this.measuring) {
-     console.error("already measuring");
-     return;
-   }
-
-   this.measuring = true;
+ init_measuring() {
    this.maxSpeed = 0;
-   this.punchDetected = false;
+
+   this.ax = this.accel.x;
+   this.ay = this.accel.y;
+   this.az = this.accel.z;
 
    this.vx = 0;
    this.vy = 0;
    this.vz = 0;
 
+   this.lx = 0;
+   this.ly = 0;
+   this.lz = 0;
+
    this.mod_v = 0;
 
-   this.ax = this.accel.x;
-   this.ay = this.accel.y;
-   this.az = this.accel.z;
    this.mod_a = calcVectorMod(
         this.accel.x, this.accel.y, this.accel.z
      );
    this.t = this.accel.timestamp;
-
-   this.accel.addEventListener('reading', this.onreading);
-   this.accel.addEventListener('error', this.onerror);
-   this.timeoutId = setTimeout(this.ontimeout, this.timeout);
    this.list_data_axis = {
      a: {x:[], y:[], z:[]},
-     v: {x:[], y:[], z:[]}
+     v: {x:[], y:[], z:[]},
+     l: {x:[], y:[], z:[]}
    };
    this.accel_mod = [];
    this.dt_list = [];
  }
 
+ start() {
+   setGameText("Started measuring");
+   if (this.timeoutId) {
+     clearTimeout(this.timeoutId);
+     this.timeoutId = 0;
+   }
+
+   if (this.timeoutId) {
+     clearTimeout(this.timeoutId);
+     this.timeoutId = 0;
+   };
+   if (this.accel.timestamp === null) {
+     setGameText("accelerometer must have initial values");
+     return;
+   };
+
+   if (this.measuring) {
+     setGameText("already measuring");
+     return;
+   };
+   // setGameText("start measuring1"+this.accel );
+   this.measuring = true;
+   this.accel.addEventListener('reading', this.onreading);
+   this.accel.addEventListener('error', this.onerror);
+   this.timeoutId = setTimeout(this.ontimeout, this.timeout);
+   this.init_measuring();
+ }
+
  stop() {
+   setGameText("Stopped measuring");
    this.measuring = false;
    if (this.timeoutId) {
      clearTimeout(this.timeoutId);
@@ -139,7 +168,6 @@ class MaxSpeedCalculator {
    }
    this.accel.removeEventListener('reading', this.onreading);
    this.accel.removeEventListener('error', this.onerror);
-
  }
 
 }
@@ -169,31 +197,20 @@ function getQuote(val) {
 }
 
 function setToInitialState() {
-  var shaking = false;
-
-  function onreading() {
-    const shakeTreashold = 3 * 9.8;
-    const stillTreashold = 1;
-    let magnitude = Math.hypot(acl.x, acl.y, acl.z);
-    if (magnitude > shakeTreashold) {
-      shaking = true;
-      setGameText(magnitude);
-    } else if (magnitude < stillTreashold && shaking) {
-      shaking = false;
-      acl.removeEventListener('reading', onreading);
-      setMeasurement(0);
-      setGameText("Punch now!");
-      speedCalculator.start();
-    }
-  }
-
-  acl.addEventListener('reading', onreading);
+    setGameText("Ready measure");
 }
 
-function onresult() {
-  setMeasurement(speedCalculator.result);
-  setGameText(getQuote(speedCalculator.result) + " Shake to try again!");
-  setTimeout(setToInitialState, 1000);
+function clear_clicked() {
+  let len_datasets = chartAxis.data.datasets.length;
+  for (var i = 0; i <= len_datasets - 1; i++ ) {
+    chartAxis.data.datasets.pop();
+  };
+  len_datasets = chartMod.data.datasets.length;
+  for (var i = 0; i <= len_datasets - 1; i++ ) {
+    chartMod.data.datasets.pop();
+  };
+  chartAxis.update();
+  chartMod.update();
 }
 
 function generateKickSound() {
@@ -214,33 +231,64 @@ function generateKickSound() {
   oscillator.stop(endTime);
 };
 
-function start_clicked() {
-    setGameText("measuring started");
-    speedCalculator.start();
+function start_measuring() {
+    speedCalculator.start()
 };
 
-function stop_clicked() {
-    var len_datasets = myChart.data.datasets.length;
+function start_clicked() {
+    setGameText("Pause");
+    timeoutId = setTimeout(start_measuring, 1000);
+};
+
+function onresult() {
+    setGameText(
+        "measuring stopped"
+    );
+    let len_datasets = chartAxis.data.datasets.length;
     for (var i = 0; i <= len_datasets - 1; i++ ) {
-        myChart.data.datasets.pop();
+        chartAxis.data.datasets.pop();
     }
-    speedCalculator.stop();
+    len_datasets = chartMod.data.datasets.length;
+    for (var i = 0; i <= len_datasets - 1; i++ ) {
+        chartMod.data.datasets.pop();
+    }
+    //speedCalculator.stop();
     var dt = (
         speedCalculator.dt_list[speedCalculator.dt_list.length - 1]
         - speedCalculator.dt_list[0]
     ) * 0.001;
     var freq = 1 / (dt / speedCalculator.dt_list.length);
-    setGameText(
-        "measuring stopped"
-    );
+
     var accel_data_correct = speedCalculator.az_data.map(
         function(element) { return element * 0.05; }
     );
     var dataset_ax = {
-      data: speedCalculator.list_data_axis.v.x,
+      data: speedCalculator.list_data_axis.a.x,
       borderColor: "red",
       fill: false
     };
+    var dataset_vx = {
+      data: speedCalculator.list_data_axis.v.x,
+      borderColor: "green",
+      fill: false
+    };
+
+    var dataset_lx = {
+      data: speedCalculator.list_data_axis.l.x,
+      borderColor: "red",
+      fill: false
+    };
+    var dataset_ly = {
+      data: speedCalculator.list_data_axis.l.y,
+      borderColor: "green",
+      fill: false
+    };
+    var dataset_lz = {
+      data: speedCalculator.list_data_axis.l.z,
+      borderColor: "blue",
+      fill: false
+    };
+
     var dataset_ay = {
       data: speedCalculator.list_data_axis.v.y,
       borderColor: "green",
@@ -262,12 +310,16 @@ function stop_clicked() {
       borderColor: "green",
       fill: false
     };
-    myChart.data.datasets.push(dataset_a);
-    myChart.data.datasets.push(dataset_v);
-//    myChart.data.datasets.push(dataset_ay);
-//    myChart.data.datasets.push(dataset_az);
-    myChart.data.labels = speedCalculator.dt_list;
-    myChart.update();
+    chartAxis.data.datasets.push(dataset_lx);
+    //  chartAxis.data.datasets.push(dataset_ly);
+    // chartAxis.data.datasets.push(dataset_lz);
+    chartAxis.data.labels = speedCalculator.dt_list;
+    chartAxis.update();
+
+    chartMod.data.datasets.push(dataset_a);
+    // chartMod.data.datasets.push(dataset_v);
+    chartMod.data.labels = speedCalculator.dt_list;
+    chartMod.update();
 };
 
 function main() {
@@ -276,21 +328,19 @@ function main() {
   start_btn = document.getElementById("start");
   console.log(start_btn);
   start_btn.addEventListener("click", start_clicked);
-  stop_btn = document.getElementById("stop");
-  console.log(stop_btn);
-  stop_btn.addEventListener("click", stop_clicked);
+  clear_btn = document.getElementById("clear");
+  clear_btn.addEventListener("click", clear_clicked);
   measurement = document.getElementById("measurement");
   setGameText(game_text.innerText);
   setMeasurement(0);
   function startApp() {
     acl = new LinearAccelerationSensor({frequency: 100});
-    speedCalculator = new MaxSpeedCalculator(acl, onresult, generateKickSound);
-
+    speedCalculator = new MainCalculator(acl, onresult, 1000);
     acl.addEventListener('activate', setToInitialState);
     acl.addEventListener('error', error => {
        setGameText("Cannot fetch data from sensor due to an error.");
     });
-    myChart = new Chart("myChart", {
+    chartAxis = new Chart("chartAxis", {
       type: "line",
       data: {
         labels: [1, 2, 3],
@@ -304,7 +354,20 @@ function main() {
         legend: {display: false}
       }
     });
-    console.log(myChart.data);
+    chartMod = new Chart("chartMod", {
+      type: "line",
+      data: {
+        labels: [1, 2, 3],
+        datasets: [{
+          data: [860, 1140, 1060],
+          borderColor: "red",
+          fill: false
+        }]
+      },
+      options: {
+        legend: {display: false}
+      }
+    });
     acl.start();
   }
   if ('LinearAccelerationSensor' in window) {
